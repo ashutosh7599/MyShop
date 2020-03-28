8168eb9e895acf5584facdf970268376e1c00b6a
ashutosh7599 <ashutosh7599@users.noreply.github.com>
2020-03-28 17:12:53 +0530

    Implemented SQL Repository

---
+++ MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,55 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepositroy<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var t = Find(id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+            dbSet.Remove(t);
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T x)
+        {
+            dbSet.Add(x);
+        }
+
+        public void Update(T x)
+        {
+            dbSet.Attach(x);
+            context.Entry(x).State = EntityState.Modified;
+        }
+    }
+}
--- MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
+using MyShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepositroy<Product>,InMemoryRepositroy<Product>>();
-            container.RegisterType<IRepositroy<ProductCategory>, InMemoryRepositroy<ProductCategory>>();
+            container.RegisterType<IRepositroy<Product>,SQLRepository<Product>>();
+            container.RegisterType<IRepositroy<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
