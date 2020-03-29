2a5e91d4e9ddf0e6c4edac37f2b63484854ba32e
ashutosh7599 <ashutosh7599@users.noreply.github.com>
2020-03-29 22:16:27 +0530

    Product Listing

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,31 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepositroy<Product> context;
+        IRepositroy<ProductCategory> productCategories;
+
+        public HomeController(IRepositroy<Product> productContext, IRepositroy<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
         }
-
+        public ActionResult Details(string id)
+        {
+            Product x = context.Find(id);
+            if(x == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(x);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
